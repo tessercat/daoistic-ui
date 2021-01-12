0139c2fd2a03132380ee054c545e7210d6f106e3
Peter Christensen <peter@daoistic.ca>
2021-01-11 18:35:02 -0700

    Refactor/improve Unihan features.

--- src/js/index.js
+++ src/js/index.js
@@ -2,24 +2,39 @@
  *  Copyright (c) 2021 Peter Christensen. All Rights Reserved.
  *  CC BY-NC-ND 4.0.
  */
-function showPanel(panel, article) {
+import logger from './logger.js';
+
+function hidePanel(panel) {
+  panel.classList.add('hide');
+  panel.classList.remove('show');
+}
+
+function showPanel(event, panel) {
+  // Load clicked-link data into the definition panel.
+  const article = document.querySelector(event.target.attributes.href.value);
   panel.innerHTML = article.outerHTML;
+  //logger.info('height', event.target.offsetHeight);
+  //logger.info('left', event.target.offsetLeft);
+  //logger.info('top', event.target.offsetTop);
+  //logger.info('width', event.target.offsetWidth);
   panel.classList.remove('hide');
   panel.classList.add('show');
 }
 
-function hidePanel(panel) {
-  panel.classList.remove('show');
-  panel.classList.add('hide');
-}
-
-document.querySelectorAll('.v-toggle').forEach((section) => {
-  section.style.display = 'none';
-});
-
-window.addEventListener('load', function () {
+function addVocabListeners() {
   const panel = document.querySelector('#definition');
+
+  // Hide the panel when the document scrolls.
+  document.addEventListener('scroll', () => {
+    hidePanel(panel);
+  });
+
+  // Hide the panel when the document is clicked, unless the click is on
+  // a Unihan character link or on the panel itself.
   document.addEventListener('click', (event) => {
+    if (panel.classList.contains('hide')) {
+      return;
+    }
     let exempt = false;
     if (
         event.target.localName === 'a'
@@ -32,13 +47,26 @@ window.addEventListener('load', function () {
       hidePanel(panel);
     }
   });
+
+  // Show the panel when a Unihan character is clicked.
   document.querySelectorAll('.unihan').forEach((link) => {
-    link.onclick = (event) => {
-      const article = document.querySelector(
-        event.target.attributes.href.value
-      );
-      showPanel(panel, article)
+    link.addEventListener('click', (event) => {
+      showPanel(event, panel);
       return false;
-    }
+    });
   });
+
+  logger.info('Vocab enabled.');
+}
+
+// Set log vars and hide vocab elements as soon as the script runs.
+document.debugLogEnabled = true;
+document.infoLogEnabled = true;
+document.querySelectorAll('.v-toggle').forEach((section) => {
+  section.style.display = 'none';
+});
+
+// Init the definitions panel when the page loads.
+window.addEventListener('load', function() {
+  addVocabListeners();
 });
