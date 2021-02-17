75c9f74568f61a19f44f10b1ec61aebd77ac1a23
Peter Christensen <peter@daoistic.ca>
2021-02-17 14:51:47 -0700

    Upgrade/fiddle.

--- deploy.bash
+++ deploy.bash
@@ -2,32 +2,32 @@
 FROM_CSS=/opt/daoistic/ui/dist/css
 FROM_JS=/opt/daoistic/ui/dist/js
 
-NGINX_CSS=/opt/daoistic/static/common/css
-rm $NGINX_CSS/daoistic*
-cp $FROM_CSS/* $NGINX_CSS
-chown www-data:www-data $NGINX_CSS/*
+STATIC_CSS=/opt/daoistic/static/common/css
+rm $STATIC_CSS/daoistic*
+cp $FROM_CSS/* $STATIC_CSS
+chown www-data:www-data $STATIC_CSS/*
 
-NGINX_JS=/opt/daoistic/static/common/js
-rm $NGINX_JS/daoistic*
-cp $FROM_JS/* $NGINX_JS
-chown www-data:www-data $NGINX_JS/*
+STATIC_JS=/opt/daoistic/static/common/js
+rm $STATIC_JS/daoistic*
+cp $FROM_JS/* $STATIC_JS
+chown www-data:www-data $STATIC_JS/*
 
-APP_CSS=/opt/daoistic/web/common/static/common/css
-rm $APP_CSS/daoistic*
-cp $FROM_CSS/* $APP_CSS
-chown peter:peter $APP_CSS/*
+COMMON_APP_CSS=/opt/daoistic/web/common/static/common/css
+rm $COMMON_APP_CSS/daoistic*
+cp $FROM_CSS/* $COMMON_APP_CSS
+chown peter:peter $COMMON_APP_CSS/*
 
-APP_JS=/opt/daoistic/web/common/static/common/js
-rm $APP_JS/daoistic*
-cp $FROM_JS/* $APP_JS
-chown peter:peter $APP_JS/*
+COMMON_APP_JS=/opt/daoistic/web/common/static/common/js
+rm $COMMON_APP_JS/daoistic*
+cp $FROM_JS/* $COMMON_APP_JS
+chown peter:peter $COMMON_APP_JS/*
 
-PROJECT_CSS=/opt/daoistic/web/static/common/css
-rm $PROJECT_CSS/daoistic*
-cp $FROM_CSS/* $PROJECT_CSS
-chown peter:peter $PROJECT_CSS/*
+COMMON_PROJECT_CSS=/opt/daoistic/web/static/common/css
+rm $COMMON_PROJECT_CSS/daoistic*
+cp $FROM_CSS/* $COMMON_PROJECT_CSS
+chown peter:peter $COMMON_PROJECT_CSS/*
 
-PROJECT_JS=/opt/daoistic/web/static/common/js
-rm $PROJECT_JS/daoistic*
-cp $FROM_JS/* $PROJECT_JS
-chown peter:peter $PROJECT_JS/*
+COMMON_PROJECT_JS=/opt/daoistic/web/static/common/js
+rm $COMMON_PROJECT_JS/daoistic*
+cp $FROM_JS/* $COMMON_PROJECT_JS
+chown peter:peter $COMMON_PROJECT_JS/*
